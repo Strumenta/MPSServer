ed8ad63f24cd5c3d0bc6d724cb6ac39f6f8c2a5a
Federico Tomassetti <federico@tomassetti.me>
2019-08-08 16:44:08 +0200

    rendering editor model

--- architecture.md
+++ architecture.md
@@ -1,4 +1,9 @@
-The client receives from the server a description of the editor it should display.
+The client receives from the server a description of the editor it should display. This is called the editor model.
 
-At that point the client performs a diff between the editor currently displayed and the one received and it
-generates the necessary DOM changes. 
+It has then to reconcile that model with the editor being currently displayed. It would do that by operating the necessary
+DOM changes.
+
+The reconcilation could be performed in two ways:
+
+1. Directly against what is currently displayed
+2. Against the last version of the editor model received
--- mpswebeditor/js/projectional_editor.js
+++ mpswebeditor/js/projectional_editor.js
@@ -1,14 +1,83 @@
 
-function editorAPI() {
+function editorAPI(editorHtmlElement) {
 
-    var currentEditorModel = {};
+    var currentEditorModel = {
+        type: "editor",
+        root: null
+    };
+
+    console.log("editorAPI " + editorHtmlElement);
+
+    function pathToCellID(path) {
+        return "cell" + path.replace("/", "_")
+    }
+
+    function renderEditor(editorModel) {
+        if (editorModel.type === "vertical_list") {
+            let html = $("<div class=\"vertical_list\" id=\"" + pathToCellID(editorModel.path) + "\"></div>");
+            $.each(editorModel.children, function (key, value) {
+                let htmlChild = renderEditor(value);
+                html.append(htmlChild);
+            });
+            return html
+        } else if (editorModel.type === "horizontal_list") {
+            let html = $("<div class=\"horizontal_list\" id=\"" + pathToCellID(editorModel.path) + "\"></div>");
+            $.each(editorModel.children, function( key, value ) {
+                let htmlChild = renderEditor(value);
+                html.append(htmlChild);
+            });
+            return html
+        } else if (editorModel.type === "constant") {
+            let html = $("<div class=\"constant " + editorModel.style + "\" contenteditable>" + editorModel.content + "</div>");
+            return html
+        } else if (editorModel.type === "string_property") {
+            let html = $("<div class=\"string_property\" contenteditable>" + editorModel.content + "</div>");
+            return html;
+        } else if (editorModel.type === "placeholder") {
+            let html = $("<div class=\"placeholder\" contenteditable>" + editorModel.content + "</div>");
+            return html;
+        } else if (editorModel.type === "spacer") {
+            let html = $("<div class=\"spacer\" contenteditable></div>");
+            return html;
+        } else {
+            throw Error(JSON.stringify(editorModel));
+        }
+    }
+
+    function updateEditorRenderingOfArray(prevArray, newArray, htmlElementOfContainer) {
+        $.each(editorModel.children, function( key, value ) {
+            console.log(value);
+        });
+    }
+
+    function reconcileRenderedEditor(prevModel, newModel, htmlElement) {
+        console.log("[updateEditorRendering] on " + htmlElement);
+        if (prevModel.type === newModel.type) {
+            if (newModel.type === "editor") {
+                if (prevModel.root !== newModel.root) {
+                    if (prevModel.root === null) {
+                        if (newModel.root !== null) {
+                            let newHtmlElement = renderEditor(newModel.root);
+                            htmlElement.append(newHtmlElement);
+                        }
+                    } else {
+                        throw Error("Unsupported");
+                    }
+                }
+            } else {
+                throw Error("Unsupported");
+            }
+        } else {
+            throw Error("Prev model had type " + prevModel.type + " new model has type " + newModel.type);
+        }
+    }
 
     function updateEditorModel(newEditorModel) {
-        updateEditorRendering(currentEditorModel, newEditorModel);
+        reconcileRenderedEditor(currentEditorModel, newEditorModel, editorHtmlElement);
         currentEditorModel = newEditorModel;
     }
 
     return {
-        "updateEditorModel" : updateEditorModel()
+        "updateEditorModel" : updateEditorModel
     };
 }
