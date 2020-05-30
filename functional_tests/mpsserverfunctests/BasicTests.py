09463d92b5868a9fbf46d43652d1ef5b2200ab40
Federico Tomassetti <federico@tomassetti.me>
2020-05-30 09:54:04 +0200

    using reload all method

--- functional_tests/mpsserverfunctests/BasicTests.py
+++ functional_tests/mpsserverfunctests/BasicTests.py
@@ -81,9 +81,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(['com.strumenta.businessorg.sandbox.acmeinc'], changedModels)
         time.sleep(1)
 
-        r = requests.post('%s/persistence/reloadAll' % BASE_URL)
-        self.assertEqual(200, r.status_code)
-        time.sleep(1)
+        self.reloadAll()
 
         r = requests.get('%s/persistence/changedModels' % BASE_URL)
         self.assertEqual(200, r.status_code)
