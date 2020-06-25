95376d2671df9168ce51495e892547adc9b67329
Federico Tomassetti <federico@tomassetti.me>
2020-06-25 14:15:05 +0200

    fix path

--- functional_tests/execute_functional_tests.sh
+++ functional_tests/execute_functional_tests.sh
@@ -1,5 +1,5 @@
 cd ..
-MPSSERVER_EXTENSION_PATH="com.strumenta" MPSSERVER_PROJECT_FILE_PATH="`pwd`/functional_tests/mps_example_project_withextension" MPSSERVER_AUTOSAVE=false MPSSERVER_PORT=7994 ./gradlew runLauncher &
+MPSSERVER_EXTENSION_PATH="ProtocolLanguage" MPSSERVER_PROJECT_FILE_PATH="`pwd`/functional_tests/mps_example_project_withextension" MPSSERVER_AUTOSAVE=false MPSSERVER_PORT=7994 ./gradlew runLauncher &
 cd functional_tests
 MPS_SERVER_PID=$!
 python -m unittest discover mpsserverfunctests -p '*Tests.py'
