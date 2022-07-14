f819b1fcf043f927232c7c70e96effd8a2bff0a0
Federico Tomassetti <federico@tomassetti.me>
2022-07-14 09:36:37 +0200

    using new database dump

--- functional_tests/execute_functional_tests.sh
+++ functional_tests/execute_functional_tests.sh
@@ -2,7 +2,7 @@ cd ..
 MPSSERVER_EXTENSION_PATH="ProtocolLanguage,com.strumenta.mpsserver.modelix" MPSSERVER_PROJECT_FILE_PATH="`pwd`/functional_tests/mps_example_project_withextension" MPSSERVER_AUTOSAVE=false MPSSERVER_PORT=7994 ./gradlew runLauncher > server_log_out.txt 2>server_log_err.txt &
 MPS_SERVER_PID=$!
 
-java -jar apps/model-server-fatjar-2021.1.116.jar -inmemory -dumpin functional_tests/modelserver_dumps/testdump1 -port 7777 &
+java -jar apps/model-server-fatjar-2021.1.116.jar -inmemory -dumpin functional_tests/modelserver_dumps/testdump2 -port 7777 &
 MODEL_SERVER_PID=$!
 
 cd functional_tests
